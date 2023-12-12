177eef6d525d64612d640982ad56b3b73f0c85d7
tiberius030 <153438924+tiberius030@users.noreply.github.com>
2023-12-12 17:57:39 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,9 +7,10 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Okamsen_vs_Schneiler += 10
 })
+let Schneiler = 0
 let Okamsen_vs_Schneiler = 0
 Okamsen_vs_Schneiler = pins.digitalReadPin(DigitalPin.P3)
 basic.showNumber(Okamsen_vs_Schneiler)
 basic.forever(function () {
-	
+    Schneiler = pins.digitalReadPin(DigitalPin.P3)
 })
