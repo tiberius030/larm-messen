fcc7572f46b3cac497b8309b54ed81f096baacec
tiberius030 <153438924+tiberius030@users.noreply.github.com>
2023-12-12 17:56:27 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Okamsen_vs_Schneiler += -10
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.showNumber(Okamsen_vs_Schneiler)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Okamsen_vs_Schneiler += 10
+})
+let Okamsen_vs_Schneiler = 0
+Okamsen_vs_Schneiler = pins.digitalReadPin(DigitalPin.P3)
+basic.showNumber(Okamsen_vs_Schneiler)
+basic.forever(function () {
+	
+})
