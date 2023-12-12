477df0d375f75202cf65efa5739401d8ec5e8c71
tiberius030 <153438924+tiberius030@users.noreply.github.com>
2023-12-12 18:22:13 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onSound(DetectedSound.Loud, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Okamsen_vs_Schneiler += -10
 })
@@ -13,4 +16,25 @@ Okamsen_vs_Schneiler = pins.digitalReadPin(DigitalPin.P3)
 basic.showNumber(Okamsen_vs_Schneiler)
 basic.forever(function () {
     Schneiler = pins.digitalReadPin(DigitalPin.P3)
+    if (Schneiler > Okamsen_vs_Schneiler) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.setLedColor(0xff0000)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.setLedColor(0x00ff00)
+        music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    }
 })
